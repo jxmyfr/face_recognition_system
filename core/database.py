95a68fefabe826884b37da3bfc1eb4a08063cdff
Jxmyfr <s.k.c.gsk@gmail.com>
2025-05-25 23:27:24 +0700

    add code in database.py

--- core/database.py
+++ core/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 import os
 from dotenv import load_dotenv
 
@@ -10,3 +11,10 @@ DB_URL = os.getenv("DB_URL")
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
