06c1340df607198b5ebb68ee5a180d8521b661dd
Jxmyfr <s.k.c.gsk@gmail.com>
2025-06-03 18:11:56 +0700

    update ไฟล์ database

--- core/auth.py
+++ core/auth.py
@@ -4,6 +4,7 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from passlib.hash import bcrypt
 from sqlalchemy.orm import Session
 from core.database import get_db
 from models import User
@@ -19,7 +20,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 # Password utils
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
     return pwd_context.hash(password)
